d8a7f849328e3027b21d44e5ab1cee62ddf6d255
Elod <elod.pro1@gmail.com>
2022-06-23 19:01:32 +0200

    v1.2

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ let ctx=canvas.getContext("2d");
 
 //Toutes les variables définies ici
 // Variable rectLeft
-xl=450;
-yl=800;
-lL=100;
-hl=200;
+let xl=20;
+let yl=20;
+let lL=35;
+let hl=250;
 
 // Variable rectRight
-xr=200
-yr=200
-lR=110 
-hr=270
+let xr=50;
+let yr=20;
+let lR=35; 
+let hr=250;
+
+//Variable du pong
+
 
 
 //Platforme de gauche
@@ -24,6 +27,10 @@ rectLeft.src="/Images/rect-white.png";
 let rectRight=new Image();
 rectRight.src="/Images/rect-white.png";
 
+//Balle pong-pong
+let pong=new Image();
+pong.src="/Images/pong.png";
+
 
 
 
@@ -33,13 +40,25 @@ let tmr=setInterval(function(){timerVariable()},40);
 function timerVariable(){
 
     
-
+    // Appel de la fonction draw pour
+    //Quelle s'éxecute
+    draw();
 }
 
 //Fonction pour dessiner toutes les choses du monde
 function draw(){
     // Fonction qui dessine la scène
     ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    // Rectangle de gauche
+	ctx.fillStyle="white";
+    ctx.drawImage(rectLeft,xl*57,yl,lL,hl);
+    
+    // Rectangle de droite
+	ctx.fillStyle="white";
+    ctx.drawImage(rectRight,xr/2,yr/1.1,lR,hr);
+
+
 }
 
 
@@ -53,21 +72,25 @@ function keyMoove(e){
 
     //touche w
     if (touche==87){
-        yl=Math.max(yv-20);
+        yl=Math.max(yl-50);
+        if(yl<-6) yl=-6;
     }
     //touche s
     if (touche==83){
-        yl=Math.max(yv+20);
+        yl=Math.max(yl+50);
+        if (yl>508) yl=508;
     }
 
     //touche flèche du haut
     if (touche==38){
-        yr=Math.max(yr-20);
+        yr=Math.max(yr-50);
+        if(yr<-6) yr=-6;
     }
 
     //touche flèche du bas
     if (touche==40){
-        yr=Math.max(yr+20);
+        yr=Math.max(yr+50);
+        if (yr>508) yr=508;
     }
 
 }
