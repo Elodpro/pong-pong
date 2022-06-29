396bf51c57b99a35b0c8ac4ac14f95aaeb387591
Elod <elod.pro1@gmail.com>
2022-06-29 21:59:56 +0200

    v1.8

--- main.js
+++ main.js
@@ -4,34 +4,31 @@ let ctx=canvas.getContext("2d");
 
 //Toutes les variables définies ici
 // Variable rectLeft
-let xl=20;
-let yl=20;
-let lL=30;
-let hl=210;
-
+var rectLeft = {
+x : 20,
+y : 20,
+l : 30,
+h : 210
+}
 // Variable rectRight
-let xr=50;
-let yr=20;
-let lR=30; 
-let hr=210;
-
+var rectRight = {
+x : 50,
+y : 20,
+l : 30, 
+h : 210
+}
 //Variable du pong
-let pRadius= 19;
-let xP = 346;
-let yP = 352;
-let vxP = 3;
-let vyP = -3;
-
+var circle = {
+r : 19,
+x : 346,
+y : 352,
+vx : 3,
+vy : -3
+}
 //Variable score
 let lScore=0;
 let rScore=0;
-//Platforme de gauche
-let rectLeft=new Image();
-rectLeft.src="/Images/rect-white.png";
 
-//Platforme de droite
-let rectRight=new Image();
-rectRight.src="/Images/rect-white.png";
 
 
 
@@ -87,58 +84,51 @@ function draw(){
     
     //dessin du pong
     ctx.beginPath();
-    ctx.arc(xP*1.72, yP, pRadius, 0, Math.PI*2);
+    ctx.arc(circle.x*1.72, circle.y, circle.r, 0, Math.PI*2);
     ctx.fillStyle="white";
     ctx.fill();
     ctx.closePath();
 
     // Rectangle de gauche
 	ctx.fillStyle="white";
-    ctx.drawImage(rectLeft,xl*1.25,yl*12.5,lL,hl);
+    ctx.fillRect(rectLeft.x*1.25,rectLeft.y*12.5,rectLeft.l,rectLeft.h);
     
     // Rectangle de droite
 	ctx.fillStyle="white";
-    ctx.drawImage(rectRight,xr*22.9,yr*12.5,lR,hr);
+    ctx.fillRect(rectRight.x*22.9,rectRight.y*12.5,rectRight.l,rectRight.h);
 
 }
 
 //Marche pas du tout
 function pongCollisionRect(){
-    if(xP + pRadius < xl + yl + lL + hl-pRadius){
-        vxP = -vxP;
-
-    }
+    
 }
 
+//Collision du ballon contre certaines surfaces du canvas
 function pongCollisionWalls(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw();
     
-    //Rebondir coté du mur
-    if(xP + vxP > canvas.height-pRadius) {
-      vxP = -vxP;
+    //Rebondir coté du mur de droite
+    if(circle.x + circle.vx > canvas.height-circle.r) {
+      circle.vx = -circle.vx;
       lScore++; 
    }
-
-   if(xP +vxP < pRadius){
-    vxP = -vxP;
+    //Rebondir coté du mur de gauche
+   if(circle.x +circle.vx < circle.r){
+    circle.vx = -circle.vx;
     rScore++;
    }
 
-    //Rebondir haut du mur
-    if(yP + vyP > canvas.height-pRadius || yP + vyP < pRadius) {
-        vyP = -vyP;
+    //Rebondir haut du mur et bas du mur
+    if(circle.y + circle.vy > canvas.height-circle.r || circle.y + circle.vy < circle.r) {
+        circle.vy = -circle.vy;
     }
     
-    xP += vxP;
-    yP += vyP;
+    circle.x += circle.vx;
+    circle.y += circle.vy;
 }
 setInterval(pongCollisionWalls, 12);
-
-
-//Collisions avec balle et rectangles
-
-
 
 //Définir les déplacement avec les touches du clavier 
 //Pour chaque rectangle blanc
@@ -150,25 +140,25 @@ function keyMoove(e){
 
     //touche w
     if (touche==87){
-        yl=Math.max(yl-5);
-        if(yl<0) yl=0;
+        rectLeft.y=Math.max(rectLeft.y-5);
+        if(rectLeft.y<0) rectLeft.y=0;
     }
     //touche s
     if (touche==83){
-        yl=Math.max(yl+5);
-        if (yl>40) yl=40;
+        rectLeft.y=Math.max(rectLeft.y+5);
+        if (rectLeft.y>40) rectLeft.y=40;
     }
 
     //touche flèche du haut
     if (touche==38){
-        yr=Math.max(yr-5);
-        if(yr<0) yr=0;
+        rectRight.y=Math.max(rectRight.y-5);
+        if(rectRight.y<0) rectRight.y=0;
     }
 
     //touche flèche du bas
     if (touche==40){
-        yr=Math.max(yr+5);
-        if (yr>40) yr=40;
+        rectRight.y=Math.max(rectRight.y+5);
+        if (rectRight.y>40) rectRight.y=40;
     }
 
 }
